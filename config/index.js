b3c65335502ff368c8bcaac0019bb53e35caeea9
JasonWang <126656711+jason-wang101843@users.noreply.github.com>
2023-07-01 14:52:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx670a0ab14b67d84a",
+  appsecret: "0b56546b1e0f5e53242044a93d0bf0e2",
+  template_id: "h3zq40kA91S3S5STU_mCkquRwGxNOcEQKERi7IoZApQ",
+  touser: "o4oIc66qNHs5zfv2kHndEa89eY2M",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "合肥",
+  first_date: "2019-10-18",
 };
